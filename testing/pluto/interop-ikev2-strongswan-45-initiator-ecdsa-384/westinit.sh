a19d7d6954dbb5c3a99d8b5fe6a51a00565f595a
Sahana Prasad <sahana.prasad07@gmail.com>
2018-08-20 01:29:30 -0500

    Testing success logs

--- testing/pluto/interop-ikev2-strongswan-45-initiator-ecdsa-384/westinit.sh
+++ testing/pluto/interop-ikev2-strongswan-45-initiator-ecdsa-384/westinit.sh
@@ -6,7 +6,4 @@ cp /testing/x509/strongswan/strongCAcert.der /etc/strongswan/ipsec.d/cacerts/
 cp /testing/x509/strongswan/strongWestCert.der /etc/strongswan/ipsec.d/certs/
 cp /testing/x509/strongswan/strongWestKey.der /etc/strongswan/ipsec.d/private/
 chmod 600 /etc/strongswan/ipsec.d/private/*
-# example import for libreswan
-/usr/bin/pk12util -i /testing/x509/strongswan/strongWest.p12 -d sql:/etc/ipsec.d -w /testing/x509/nss-pw
-../../pluto/bin/strongswan-start.sh
 echo "initdone"
